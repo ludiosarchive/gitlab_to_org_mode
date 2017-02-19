54d0f416d993f6e6d1567bc6f0f3bcd562018606
Ivan Kozik <ivan@ludios.org>
2017-02-19 21:42:03 +0000

    We have no files/

--- lib/role.ex
+++ lib/role.ex
@@ -1,9 +1,4 @@
-alias Converge.Util
-
 defmodule RoleXfs do
-	require Util
-	Util.declare_external_resources("files")
-
 	def role(_tags \\ []) do
 		%{
 			desired_packages: ["xfsprogs", "xfsdump"]
