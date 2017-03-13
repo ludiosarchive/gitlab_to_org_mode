d0c9d4e8d46eb17b89ce2ad3ca361dd9e978d7ef
Ivan Kozik <ivan@ludios.org>
2017-03-13 00:10:44 +0000

    role_grab_site

--- lib/role.ex
+++ lib/role.ex
@@ -1,13 +1,13 @@
-defmodule RoleWebTesting do
+defmodule RoleGrabSite do
 	def role(_tags \\ []) do
 		%{
 			desired_packages: [
-				"google-chrome-stable",
-				"google-chrome-beta",
-				"google-chrome-unstable",
-				"firefox",
+				"git",
+				"build-essential",
+				"python3.4",
+				"python3.4-dev",
+				"python3.4-venv",
 			],
-			implied_roles:    [RoleGoogleChromeRepo],
 		}
 	end
 end
--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule RoleWebTesting.Mixfile do
+defmodule RoleGrabSite.Mixfile do
 	use Mix.Project
 
 	def project do
 		[
-			app:             :role_web_testing,
+			app:             :role_grab_site,
 			version:         "0.1.0",
 			elixir:          ">= 1.4.0",
 			build_embedded:  Mix.env == :prod,
@@ -14,8 +14,7 @@ defmodule RoleWebTesting.Mixfile do
 
 	defp deps do
 		[
-			{:converge,                ">= 0.1.0"},
-			{:role_google_chrome_repo, ">= 0.1.0"},
+			{:converge, ">= 0.1.0"},
 		]
 	end
 end
